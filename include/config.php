0eaa362b63d2d5dba661cc050b79963feaeb75ba
khaerululum <67420471+khaerululum@users.noreply.github.com>
2020-10-12 11:49:43 +0700

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,6 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<admin','mikhmon>|>mZWfoZ+rmaSTsZI=');
+
+$data['khatulistiwahotspotrb751'] = array ('1'=>'khatulistiwahotspotrb751!192.168.1.2','khatulistiwahotspotrb751@|@ulum','khatulistiwahotspotrb751#|#mZWfoZ+rmaSTsZI=','khatulistiwahotspotrb751%khatulistiwahotspot','khatulistiwahotspotrb751^khatulistiwahotspot.net','khatulistiwahotspotrb751&Rp','khatulistiwahotspotrb751*10','khatulistiwahotspotrb751(1','khatulistiwahotspotrb751)','khatulistiwahotspotrb751=10','khatulistiwahotspotrb751@!@enable');
+$data['rb800'] = array ('1'=>'rb800!192.168.3.1','rb800@|@ulum','rb800#|#mZWfoZ+rmaSTsZI=','rb800%khatulistiwahotspot','rb800^khatulistiwahotspot.net','rb800&Rp','rb800*10','rb800(1','rb800)','rb800=10','rb800@!@enable');
